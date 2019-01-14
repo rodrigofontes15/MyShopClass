86bf51eb8864755ab914fe2eeaae22a21d156d2a
rodrigofontes15 <rodrigofontes15@users.noreply.github.com>
2019-01-14 23:41:27 +0100

    Order Management pages

--- MyShop/MyShop.WebUI/Controllers/BasketController.cs
+++ MyShop/MyShop.WebUI/Controllers/BasketController.cs
@@ -10,15 +10,16 @@ namespace MyShop.WebUI.Controllers
 {
     public class BasketController : Controller
     {
-
+        IRepository<Customers> customers;
         IBasketService basketService;
         IOrderService orderService;
 
-        public BasketController(IBasketService BasketService, IOrderService OrderService)
+        public BasketController(IBasketService BasketService, IOrderService OrderService, IRepository<Customers> Customers)
         {
             this.basketService = BasketService;
             this.orderService = OrderService;
-      
+            this.customers = Customers;
+
         }
 
         // GET: Basket
@@ -27,7 +28,7 @@ namespace MyShop.WebUI.Controllers
 
             var model = basketService.GetBasketItems(this.HttpContext);
             return View(model);
-                       
+
         }
 
         public ActionResult AddToBasket(string Id)
@@ -51,17 +52,42 @@ namespace MyShop.WebUI.Controllers
             return PartialView(basketSumary);
 
         }
-
+        [Authorize]
         public ActionResult Checkout()
         {
-            return View();
+            Customers customer = customers.Collection().FirstOrDefault(c => c.Email == User.Identity.Name);
+
+
+            if (customer != null)
+            {
+                Order order = new Order()
+                {
+                    Email = customer.Email,
+                    City = customer.City,
+                    State = customer.State,
+                    Street = customer.Street,
+                    FirstName = customer.FirstName,
+                    LastName = customer.LastName,
+                    ZipCode = customer.ZipCode
+                };
+
+                return View(order);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
         }
 
+
+
         [HttpPost]
+        [Authorize]
         public ActionResult Checkout(Order order)
         {
             var basketItems = basketService.GetBasketItems(this.HttpContext);
             order.OrderStatus = "Order Created";
+            order.Email = User.Identity.Name;
 
             //process payment
 
